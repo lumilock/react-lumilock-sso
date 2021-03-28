1b6d4b4aa179c0b26894f15e026ff1612971d491
lumilock <thibaud.perrin@outlook.com>
2021-03-28 17:09:26 +0200

    export authSelectors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import ProtectedRoutes from './components/ProtectedRoutes';
 import GuestRoutes from './components/GuestRoutes';
 import LoginForm from './components/form/LoginForm';
 import authReducer from './store/reducers/authReducer';
+import {
+  authFullSelector, authSelector, expireSelector, loginSelector,
+} from './store/selectors/authSelectors';
 
 export {
-  LumilockProvider, ProtectedRoutes, GuestRoutes, LoginForm, authReducer,
+  LumilockProvider, ProtectedRoutes, GuestRoutes, LoginForm, authReducer, authFullSelector, authSelector, expireSelector, loginSelector,
 };
